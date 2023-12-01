b4b318684a4bac532a48aab133ec117ed3a35484
Salih <salihkm000@gmail.com>
2023-12-01 10:40:37 +0530

    about and experience sections updated

--- Js/main.js
+++ Js/main.js
@@ -1,34 +1,57 @@
-const multipleItemCarousel = document.querySelector(
-    "#carouselExampleControls"
-  );
+// const multipleItemCarousel = document.querySelector(
+//     "#carouselExampleControls"
+//   );
 
-if(window.matchMedia("(min-width :576px)").matches){
+// if(window.matchMedia("(min-width :576px)").matches){
 
-    const carousel = new bootstrap.Carousel(multipleItemCarousel ,{
-        interval : false
-    })
+//     const carousel = new bootstrap.Carousel(multipleItemCarousel ,{
+//         interval : false
+//     })
 
-    var carouselWidth = $(".carousel-inner")[0].scrollWidth;
-    var cardWidth = $(".carousel-item").width();
-    var scrollPosition = 0;
+//     var carouselWidth = $(".carousel-inner")[0].scrollWidth;
+//     var cardWidth = $(".carousel-item").width();
+//     var scrollPosition = 0;
     
-    $(".carousel-control-next").on("click", function () {
-        if (scrollPosition < (carouselWidth - cardWidth * 3)) { //check if you can go any further
-          scrollPosition += cardWidth;  //update scroll position
-          $(".carousel-inner").animate({ scrollLeft: scrollPosition },600); //scroll left
-        }
-      });
+//     $(".carousel-control-next").on("click", function () {
+//         if (scrollPosition < (carouselWidth - cardWidth * 3)) { //check if you can go any further
+//           scrollPosition += cardWidth;  //update scroll position
+//           $(".carousel-inner").animate({ scrollLeft: scrollPosition },600); //scroll left
+//         }
+//       });
     
-      $(".carousel-control-prev").on("click", function () {
-        if (scrollPosition > 0) {
-          scrollPosition -= cardWidth;
-          $(".carousel-inner").animate(
-            { scrollLeft: scrollPosition },
-            600
-          );
-        }
-      });
-}else{
-    $(multipleItemCarousel).addClass('slide')
+//       $(".carousel-control-prev").on("click", function () {
+//         if (scrollPosition > 0) {
+//           scrollPosition -= cardWidth;
+//           $(".carousel-inner").animate(
+//             { scrollLeft: scrollPosition },
+//             600
+//           );
+//         }
+//       });
+// }else{
+//     $(multipleItemCarousel).addClass('slide')
+// }
+
+
+const initSlider = () => {
+  const cardList = document.querySelector('.card-wrapper')
+  const sliderButtons = document.querySelectorAll('.slide-btn')
+  const maxScrolLeft = cardList.scrollWidth - cardList.clientWidth
+
+  sliderButtons.forEach(button => {
+      button.addEventListener("click",() => {
+          const direction = button.id === 'prev-slid' ? -1 : 1;
+          const scrollAmount = cardList.clientWidth * direction;
+          cardList.scrollBy({ left : scrollAmount, behavior : 'smooth'})
+      })
+  })
+  const handleSlideButtons = () => {
+      sliderButtons[0].style.display = cardList.scrollLeft <= 0 ? 'none' : 'block';
+      sliderButtons[1].style.display = cardList.scrollLeft >= maxScrolLeft ? 'none' : 'block';
+  }
+  
+  cardList.addEventListener('scroll' , ()=> {
+      handleSlideButtons();
+  })
 }
-
+window.addEventListener("load",initSlider)

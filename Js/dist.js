8d63050e07740f266151cd277b3decbd11d9021f
Salih <salihkm000@gmail.com>
2023-12-04 22:48:12 +0530

    Districts js updated

--- Js/dist.js
+++ Js/dist.js
@@ -16,10 +16,10 @@ const mainImgTwo = document.getElementById('mainImgTwo')
 const smallImgTow = document.getElementsByClassName('smallImgTow')
 console.log(mainImgTwo,smallImgTow)
 
-smallImgTow[0].onclick = () => mainImgTwo.src = smallImgTow[0].src;
-smallImgTow[1].onclick = () => mainImgTwo.src = smallImgTow[1].src;
-smallImgTow[2].onclick = () => mainImgTwo.src = smallImgTow[2].src;
-smallImgTow[3].onclick = () => mainImgTwo.src = smallImgTow[3].src;
+smallImgTow[0].onmouseover = () => mainImgTwo.src = smallImgTow[0].src;
+smallImgTow[1].onmouseover = () => mainImgTwo.src = smallImgTow[1].src;
+smallImgTow[2].onmouseover = () => mainImgTwo.src = smallImgTow[2].src;
+smallImgTow[3].onmouseover = () => mainImgTwo.src = smallImgTow[3].src;
 
 // ---------------place Three img ----------------
 
@@ -27,10 +27,10 @@ const mainImgThree = document.getElementById('mainImgThree')
 const smallImgThree = document.getElementsByClassName('smallImgThree')
 console.log(mainImgThree,smallImgThree)
 
-smallImgThree[0].onclick = () => mainImgThree.src = smallImgThree[0].src;
-smallImgThree[1].onclick = () => mainImgThree.src = smallImgThree[1].src;
-smallImgThree[2].onclick = () => mainImgThree.src = smallImgThree[2].src;
-smallImgThree[3].onclick = () => mainImgThree.src = smallImgThree[3].src;
+smallImgThree[0].onmouseover = () => mainImgThree.src = smallImgThree[0].src;
+smallImgThree[1].onmouseover = () => mainImgThree.src = smallImgThree[1].src;
+smallImgThree[2].onmouseover = () => mainImgThree.src = smallImgThree[2].src;
+smallImgThree[3].onmouseover = () => mainImgThree.src = smallImgThree[3].src;
 
 // ---------------place Four img ----------------
 
@@ -38,8 +38,8 @@ const mainImgFour = document.getElementById('mainImgFour')
 const smallImgFour = document.getElementsByClassName('smallImgFour')
 console.log(mainImgFour,smallImgFour)
 
-smallImgFour[0].onclick = () => mainImgFour.src = smallImgFour[0].src;
-smallImgFour[1].onclick = () => mainImgFour.src = smallImgFour[1].src;
-smallImgFour[2].onclick = () => mainImgFour.src = smallImgFour[2].src;
-smallImgFour[3].onclick = () => mainImgFour.src = smallImgFour[3].src;
+smallImgFour[0].onmouseover = () => mainImgFour.src = smallImgFour[0].src;
+smallImgFour[1].onmouseover = () => mainImgFour.src = smallImgFour[1].src;
+smallImgFour[2].onmouseover = () => mainImgFour.src = smallImgFour[2].src;
+smallImgFour[3].onmouseover = () => mainImgFour.src = smallImgFour[3].src;
 
